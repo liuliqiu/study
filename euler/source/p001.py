5af4369115a71b5563259429f619e8cfd5a7d163
omar <omar@meilapp.com>
2016-11-08 15:19:03 +0800

    refactoring: code style

--- euler/source/p001.py
+++ euler/source/p001.py
@@ -1,15 +1,24 @@
-##If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-##
-##Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
 
-def f1(n):
-    return sum(i for i in range(n) if i%3==0 or i%5==0)
+from __future__ import division
 
-def f(n):
-    Sum = lambda x:(x*x+x)//2
-    SumDivisibleBy = lambda k:Sum((n-1)//k)*k
-    return SumDivisibleBy(3)+SumDivisibleBy(5)-SumDivisibleBy(15)
 
-test_case = [(10, 23)]
-x = 1000
+def simple_sum_multiples_3_5_to(limit):
+    return sum(i for i in range(limit) if i % 3 == 0 or i % 5 == 0)
+
+
+def sum_multiples_3_5_to(limit):
+    """
+    >>> sum_multiples_3_5_to(10)
+    23
+    >>> sum_multiples_3_5_to(1000)
+    233168
+    """
+    sum_to = lambda x: (x * x + x) // 2
+    sum_divisible_by = lambda k: sum_to((limit - 1) // k) * k
+    return sum_divisible_by(3) + sum_divisible_by(5) - sum_divisible_by(15)
 
